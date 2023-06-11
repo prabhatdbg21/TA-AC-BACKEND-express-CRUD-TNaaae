52f7afba917721c5ecde00f0e3eb4d3040967e13
Prabhat <prabhatdbg21@gmail.com>
2023-06-11 16:23:09 +0530

    block-BNaacy

--- block-BNaacy/sample/app.js
+++ block-BNaacy/sample/app.js
@@ -1,11 +1,12 @@
 // requires
 var express = require('express');
 var mongoose = require('mongoose');
+var path = require('path');
 
 var userRouter = require('./routes/users');
 
 // connect to database
-mongoose.connect('mongodb://127.0.0.1:27017/bookstore')
+mongoose.connect('mongodb://127.0.0.1:27017/users')
 .then(() => console.log('Connected'))
 .catch((error) => console.log(error))
 
@@ -14,7 +15,10 @@ var app = express();
 
 // setup view engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views")); // all views present in views directery
+app.set('views',  path.join(__dirname, "views")); // all views present in views directery
+
+// capture form data
+app.use(express.urlencoded ({extended: false})) 
 
 // routing middlewares
 app.use('/users', userRouter);
@@ -24,53 +28,3 @@ app.listen(3000, () => {
     console.log('server is listening on port 3k')
 })
 
-
-
-
-
-/*
-// requires
-var express = require('express');
-var mongoose = require('mongoose');
-var path = require('path');
-
-var indexRouter = require('./routes/index');
-var booksRouter = require('./routes/books');
-
-// connect to database
-mongoose.connect('mongodb://127.0.0.1:27017/bookstore')
-.then(() => console.log('Connected'))
-.catch((error) => console.log(error))
-
-// instantiate the app
-var app = express();
-
-// setup view engine
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views")); // all views present in views directery
-
-// capture form data
-app.use(express.urlencoded ({extended: false}))   // urlencoded to capture form data  // extended: true for nested form   // capture the data from form and keep it into req.body 
-
-// setup static directory                    // for adding css and image
-app.use(express.static(path.join(__dirname, "public")));
-
-// routing middlewares
-app.use('/', indexRouter);
-app.use('/books', booksRouter);
-
-// Error handler middlewares
-// 404
-app.use((req, res, next) => {
-    res.send('Page not Found');
-})
-// custome error handler middleware
-app.use((err, req, res, next) => {
-    res.send(err);
-})
-
-// listener
-app.listen(3000, () => {
-    console.log('server is listening on port 3k')
-})
-*/
--- block-BNaacy/sample/models/user.js
+++ block-BNaacy/sample/models/user.js
@@ -3,9 +3,8 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     name: { type: String, required: true },  // any book must have title
-    subject: String,
-    marks: Number,
-    result: String
+    email: String,
+    age: Number
 }, {timestamps: true}) // give us created and updated time
 
 var User = mongoose.model('User' , userSchema);  // models name must be singular and start with capital letter
@@ -13,20 +12,3 @@ var User = mongoose.model('User' , userSchema);  // models name must be singular
 module.exports = User;
 
 
-
-
-/*
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var bookSchema = new Schema({
-    title: { type: String, required: true },  // any book must have title
-    description: String,
-    price: Number,
-    author: String
-}, {timestamps: true}) // give us created and updated time
-
-var Book = mongoose.model('Book' , bookSchema);  // models name must be singular and start with capital letter
-
-module.exports = Book;
-*/
